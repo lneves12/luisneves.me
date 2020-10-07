e2b501a286edd42209153357e059b14042a362b8
Luis Neves <luis.neves12@gmail.com>
2020-10-08 00:19:58 +0100

    Theme fixes + post-card improvements + package lock

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,11 @@
 import React from "react"
 import {Link} from "gatsby"
-import { RiHeart2Line } from "react-icons/ri";
+import { RiTwitterLine } from "react-icons/ri";
 
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
+      <RiTwitterLine />
       <p><Link to="/">luisneves.me</Link></p>
     </div>
   </footer>
--- src/components/post-card.js
+++ src/components/post-card.js
@@ -2,26 +2,37 @@ import React from "react"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
 
-const PostCard = ({ data }) => (
-  <article className="post-card">
-    {data.frontmatter.featuredImage ? 
-      (
-        <Link to={data.frontmatter.slug}>
-          <Img 
-            fluid={data.frontmatter.featuredImage.childImageSharp.fluid} 
-            objectFit="cover"
-            objectPosition="50% 50%"
-            alt={data.frontmatter.title + ' - Featured image'}
-            className="featured-image"
-          />
-        </Link>
-      ) : ""
-    }
-    <div class="post-content">
-      <h2 className="title"><Link to={data.frontmatter.slug}>{data.frontmatter.title}</Link></h2>
-      <p className="meta"><time>{data.frontmatter.date}</time></p>
-    </div>
-  </article>
-)
+const PostCard = ({ data }) => {
+
+  let featuredContent;
+  if(data.frontmatter.featuredImage) {
+    featuredContent = <Link to={data.frontmatter.slug}>
+      <Img
+        fluid={data.frontmatter.featuredImage.childImageSharp.fluid}
+        objectFit="cover"
+        objectPosition="50% 50%"
+        alt={data.frontmatter.title + ' - Featured image'}
+        className="featured-image"
+      />
+    </Link>
+  } else {
+    featuredContent = <Link className="description" to={data.frontmatter.slug}>{data.excerpt}</Link>;
+  }
+
+
+  return (
+    <article className="post-card">
+
+
+      <div class="post-content">
+        <h2 className="title"><Link to={data.frontmatter.slug}>{data.frontmatter.title}</Link></h2>
+        <p className="meta">
+          <time>{data.frontmatter.date}</time>
+        </p>
+      </div>
+      {featuredContent}
+    </article>
+  )
+}
 
 export default PostCard
